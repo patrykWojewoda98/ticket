e10ee59e61df2ddb9e828bbda832a6a4c6886d6f
Kacper <xnelfox@gmail.com>
2026-03-10 11:42:37 +0100

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,8 @@ import Header from "@/components/header";
 import Image from "next/image";
 
 export default function Home() {
-  
   return (
     <>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
     </>
   );
 }
