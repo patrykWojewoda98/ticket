721049fd9c756f730ab2ae0fcca2648115d8130d
tcgawel <mateusz.gawel08@gmail.com>
2026-03-11 13:25:34 +0100

    footer oraz layout
    dodanie footera oraz zmiana wyglądu layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from "react";
 import { Poppins } from "next/font/google";
-import Header from "@/components/layout/header";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 import "./globals.css";
+
 const poppins = Poppins({
   subsets: ["latin", "latin-ext"],
   weight: ["400", "700"],
@@ -12,17 +14,23 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-export default async function Layout({ children }: LayoutProps) {
-  // const session = await auth.api.getSession({ headers: await headers() });
-
+export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="pl">
       <body className={poppins.className}>
-        <div className="mx-auto px-[10%] 2xl:px-32 min-w-75 max-w-325">
-          <div className="gap-8 sm:gap-16 md:gap-24 lg:gap-32 grid">
-            <Header />
-            <main>{children}</main>
-          </div>
+        
+        <div className="flex flex-col min-h-screen mx-auto px-[10%] 2xl:px-32 min-w-75 max-w-325">
+          
+          
+          <Header />
+
+        
+          <main className="flex-grow">
+            {children}
+          </main>
+
+          
+          <Footer />
         </div>
       </body>
     </html>
