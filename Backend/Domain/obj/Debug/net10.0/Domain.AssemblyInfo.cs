64c133586d2d83f4b74696e01a15179badd1bc91
Kacper <xnelfox@gmail.com>
2026-03-18 10:39:57 +0100

    .

--- Backend/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Backend/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb072c65003b07b173b26295d0e22df5ed8287d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dee5d07bb79d37cce8ef1eafbecec0b66eae030")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
